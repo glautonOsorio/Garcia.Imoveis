223b5be52b06601ea927f3a579805194123bb35a
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-08-08 16:26:32 -0300

    pagina de produto comentada para estudo

--- Diversão/src/pages/PaginaDeProdutos/PaginaDeProdutos.jsx
+++ Diversão/src/pages/PaginaDeProdutos/PaginaDeProdutos.jsx
@@ -6,60 +6,72 @@ import BuscaProduto from "../../components/BuscaProduto/BuscaProduto.component";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 
-
 const PaginaDeProdutos = () => {
+  // Estado para armazenar a lista completa de produtos
   const [items, setItems] = useState([""]);
+
+  // Estado para armazenar o termo de pesquisa
   const [searchTerm, setSearchTerm] = useState("");
+
+  // Estado para armazenar a lista de produtos filtrados
   const [filteredItems, setFilteredItems] = useState([]);
 
+  // Carregar a lista completa de produtos quando o componente montar
   useEffect(() => {
     fetchProdutos();
   }, []);
 
+  // Função para buscar e configurar a lista de produtos
   const fetchProdutos = async () => {
     const response = await fetch("http://localhost:4000/produtos");
     const produtos = await response.json();
+
+    // Configurar a lista completa de produtos e produtos filtrados
     setItems(produtos);
-    setFilteredItems(produtos); //filtra produtos da busca
+    setFilteredItems(produtos); // Inicialmente, todos os produtos são exibidos
     return produtos;
   };
 
+  // Função para lidar com a busca de produtos
   const handleSearch = (event) => {
     const searchTerm = event.target.value;
     setSearchTerm(searchTerm);
 
+    // Filtrar os produtos com base no termo de pesquisa
     const filtered = items.filter((item) =>
       item.nome.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
+    // Configurar a lista de produtos filtrados
     setFilteredItems(filtered);
   };
 
-
   return (
     <>
-    <Header/>
-    <StyledContent>
-    <BuscaProduto searchTerm={searchTerm} onSearchChange={handleSearch} />
-    </StyledContent>
-    <StyledProduto>
-    {filteredItems.length > 0 ? (
-      filteredItems.map((item) => {
-        return (
-          <CardProduto
-            item={item}
-            key={item.id || "0"}
-          />
-        );
-      })
-    ) : (
-      <div>Não tem produto</div>
-    )}
-  </StyledProduto>
-  <Footer/>
-  </>
+      <Header /> {/* Componente de cabeçalho */}
+      <StyledContent>
+        <BuscaProduto searchTerm={searchTerm} onSearchChange={handleSearch} /> {/* Componente de busca */}
+      </StyledContent>
+      <StyledProduto>
+        {/* Verifica se existem produtos filtrados para exibir */}
+        {filteredItems.length > 0 ? (
+          // Mapeia e renderiza os produtos filtrados
+          filteredItems.map((item) => {
+            return (
+              <CardProduto
+                item={item}
+                key={item.id || "0"} // Define a chave como o ID do produto, ou "0" se não houver ID
+              />
+            );
+          })
+        ) : (
+          // Caso não haja produtos filtrados, exibe a mensagem
+          <div>Nenhum produto encontrado com este nome</div>
+        )}
+      </StyledProduto>
+      <Footer /> {/* Componente de rodapé */}
+    </>
   );
 };
-
 
 export default PaginaDeProdutos;
