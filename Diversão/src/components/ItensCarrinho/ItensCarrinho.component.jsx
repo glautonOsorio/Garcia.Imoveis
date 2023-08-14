a93846a0ab0a6cd5b7bd602d95f4bf7a2cd2f37b
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-08-14 18:37:38 -0300

    itens carrinho atualizado para styled component

--- Diversão/src/components/ItensCarrinho/ItensCarrinho.component.jsx
+++ Diversão/src/components/ItensCarrinho/ItensCarrinho.component.jsx
@@ -1,5 +1,14 @@
-import { useContext } from 'react';
-import './ItensCarrinho.style.css';
+import  { useContext } from 'react';
+import {
+    CartSection,
+    CartItems,
+    CartItem,
+    CartItemImage,
+    CartItemContent,
+    CartItemTitle,
+    CartItemPrice,
+    CartResume
+} from './ItensCarrinho.style';
 import AppContext from '../../contexts/AppContext';
 
 function ItensCarrinho() {
@@ -8,40 +17,34 @@ function ItensCarrinho() {
     const totalPrice = produtos.reduce((acc, item) => {
         const priceValue = parseFloat(item.preço.replace('R$', '').replace(',', '.')); // Converte o preço para número
         return priceValue + acc;
-      }, 0);
-      
-      const formattedTotalPrice = `R$${totalPrice.toFixed(2).replace('.', ',')}`; // Formata o preço novamente para exibição
-      
-      console.log(formattedTotalPrice); // Exibirá o preço total formatado corretamente
+    }, 0);
 
+    const formattedTotalPrice = `R$${totalPrice.toFixed(2).replace('.', ',')}`; // Formata o preço novamente para exibição
 
     return (
-        <section className={`cart ${isCartVisible ? 'cart--active' : ''}`}>
-            <div className="cart-items">
-                    {produtos.length > 0 ? (
-                    // Mapeia e renderiza os produtos no carrinho
-                    produtos.map((item) => {
-                        return (
-                            <section key={item.id} className="cart-item">
-                                <img
-                                    src={item.imagem}
-                                    alt="imagem do produto"
-                                    className="cart-item-image"
-                                />
-                                <div className="cart-item-content">
-                                    <h3 className="cart-item-title">{item.nome}</h3>
-                                    <h3 className="cart-item-price">{item.preço}</h3>
-                                </div>
-                            </section>
-                        );
-                    })
+        <CartSection className={`cart ${isCartVisible ? 'cart--active' : ''}`}>
+            <CartResume className="cart-resume">Total: {formattedTotalPrice}</CartResume>
+            <CartItems>
+                {/* Renderize os itens do carrinho */}
+                {produtos.length > 0 ? (
+                    produtos.map((item) => (
+                        <CartItem key={item.id} className="cart-item">
+                            <CartItemImage
+                                src={item.imagem}
+                                alt="imagem do produto"
+                                className="cart-item-image"
+                            />
+                            <CartItemContent className="cart-item-content">
+                                <CartItemTitle className="cart-item-title">{item.nome}</CartItemTitle>
+                                <CartItemPrice className="cart-item-price">{item.preço}</CartItemPrice>
+                            </CartItemContent>
+                        </CartItem>
+                    ))
                 ) : (
-                    // se Vazio
                     <div>Carrinho vazio</div>
                 )}
-            </div>
-            <div className="cart-resume">Total: {formattedTotalPrice}</div>
-        </section>
+            </CartItems>
+        </CartSection>
     );
 }
 
