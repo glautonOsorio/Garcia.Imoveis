479226c14edf4d086fa39612cdc3812e6b846c84
Glauton Osório <glautonleao@gmail.com>
2023-08-14 04:21:04 -0300

    começando nova pagina de carrinho

---
+++ Diversão/src/pages/PaginaDoCarrinhoGlauton/PaginaDoCarrinho.jsx
@@ -0,0 +1,13 @@
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+
+const PaginaDoCarrinho = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default PaginaDoCarrinho;
